73790ae95dc68a32a9cb6b112ba390993da8f4cb
dramirez <dramirezrrzz@gmail.com>
2025-01-21 20:26:45 -0600

    Evitando duplicados y sumando las cantidades

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,8 +5,19 @@ export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
   const addItem = (item: MenuItem) => {
-    const newItem: OrderItem = { ...item, quantity: 1 };
-    setOrder([...order, newItem]);
+    const itemExist = order.find((orderItem) => orderItem.id === item.id); // nos dice si lo encontro o NO
+    if (itemExist) {
+      const updateOrder = order.map(
+        (orderItem) =>
+          orderItem.id === item.id
+            ? { ...orderItem, quantity: orderItem.quantity + 1 } // toma una copia de lo que existe en la orden y suma una a cantidad
+            : orderItem // si no mantiene la orden
+      );
+      setOrder(updateOrder); // actualizamos el setOrder
+    } else {
+      const newItem: OrderItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
   };
 
   return {
